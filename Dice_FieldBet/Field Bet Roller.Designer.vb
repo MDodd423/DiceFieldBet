1b5f988a3509949ed76ab4076b98b15ce78dc8ea
Michael Dodd <MichaelDodd@dodd.tech>
2021-01-08 17:09:51 -0600

    Visual Studio commit

---
+++ Dice_FieldBet/Field Bet Roller.Designer.vb
@@ -0,0 +1,522 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label36 = New System.Windows.Forms.Label()
+        Me.Label35 = New System.Windows.Forms.Label()
+        Me.Label34 = New System.Windows.Forms.Label()
+        Me.Label33 = New System.Windows.Forms.Label()
+        Me.Label32 = New System.Windows.Forms.Label()
+        Me.Label31 = New System.Windows.Forms.Label()
+        Me.Label30 = New System.Windows.Forms.Label()
+        Me.Label29 = New System.Windows.Forms.Label()
+        Me.Label28 = New System.Windows.Forms.Label()
+        Me.Label27 = New System.Windows.Forms.Label()
+        Me.Label26 = New System.Windows.Forms.Label()
+        Me.Label25 = New System.Windows.Forms.Label()
+        Me.Label24 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label23 = New System.Windows.Forms.Label()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label37 = New System.Windows.Forms.Label()
+        Me.Label38 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label36
+        '
+        Me.Label36.AutoSize = True
+        Me.Label36.Location = New System.Drawing.Point(230, 220)
+        Me.Label36.Name = "Label36"
+        Me.Label36.Size = New System.Drawing.Size(13, 13)
+        Me.Label36.TabIndex = 77
+        Me.Label36.Text = "0"
+        '
+        'Label35
+        '
+        Me.Label35.Location = New System.Drawing.Point(175, 220)
+        Me.Label35.Name = "Label35"
+        Me.Label35.Size = New System.Drawing.Size(37, 13)
+        Me.Label35.TabIndex = 76
+        Me.Label35.Text = "Result"
+        '
+        'Label34
+        '
+        Me.Label34.AutoSize = True
+        Me.Label34.Location = New System.Drawing.Point(230, 194)
+        Me.Label34.Name = "Label34"
+        Me.Label34.Size = New System.Drawing.Size(13, 13)
+        Me.Label34.TabIndex = 75
+        Me.Label34.Text = "0"
+        '
+        'Label33
+        '
+        Me.Label33.Location = New System.Drawing.Point(175, 194)
+        Me.Label33.Name = "Label33"
+        Me.Label33.Size = New System.Drawing.Size(31, 13)
+        Me.Label33.TabIndex = 74
+        Me.Label33.Text = "Total"
+        '
+        'Label32
+        '
+        Me.Label32.AutoSize = True
+        Me.Label32.Location = New System.Drawing.Point(230, 181)
+        Me.Label32.Name = "Label32"
+        Me.Label32.Size = New System.Drawing.Size(13, 13)
+        Me.Label32.TabIndex = 73
+        Me.Label32.Text = "0"
+        '
+        'Label31
+        '
+        Me.Label31.AutoSize = True
+        Me.Label31.Location = New System.Drawing.Point(230, 168)
+        Me.Label31.Name = "Label31"
+        Me.Label31.Size = New System.Drawing.Size(13, 13)
+        Me.Label31.TabIndex = 72
+        Me.Label31.Text = "0"
+        '
+        'Label30
+        '
+        Me.Label30.AutoSize = True
+        Me.Label30.Location = New System.Drawing.Point(230, 155)
+        Me.Label30.Name = "Label30"
+        Me.Label30.Size = New System.Drawing.Size(13, 13)
+        Me.Label30.TabIndex = 71
+        Me.Label30.Text = "0"
+        '
+        'Label29
+        '
+        Me.Label29.AutoSize = True
+        Me.Label29.Location = New System.Drawing.Point(230, 129)
+        Me.Label29.Name = "Label29"
+        Me.Label29.Size = New System.Drawing.Size(13, 13)
+        Me.Label29.TabIndex = 70
+        Me.Label29.Text = "0"
+        '
+        'Label28
+        '
+        Me.Label28.Location = New System.Drawing.Point(175, 181)
+        Me.Label28.Name = "Label28"
+        Me.Label28.Size = New System.Drawing.Size(33, 13)
+        Me.Label28.TabIndex = 69
+        Me.Label28.Text = "Triple"
+        '
+        'Label27
+        '
+        Me.Label27.Location = New System.Drawing.Point(175, 168)
+        Me.Label27.Name = "Label27"
+        Me.Label27.Size = New System.Drawing.Size(41, 13)
+        Me.Label27.TabIndex = 68
+        Me.Label27.Text = "Double"
+        '
+        'Label26
+        '
+        Me.Label26.Location = New System.Drawing.Point(175, 155)
+        Me.Label26.Name = "Label26"
+        Me.Label26.Size = New System.Drawing.Size(36, 13)
+        Me.Label26.TabIndex = 67
+        Me.Label26.Text = "Single"
+        '
+        'Label25
+        '
+        Me.Label25.Location = New System.Drawing.Point(175, 129)
+        Me.Label25.Name = "Label25"
+        Me.Label25.Size = New System.Drawing.Size(30, 13)
+        Me.Label25.TabIndex = 66
+        Me.Label25.Text = "Lose"
+        '
+        'Label24
+        '
+        Me.Label24.AutoSize = True
+        Me.Label24.Location = New System.Drawing.Point(192, 103)
+        Me.Label24.Name = "Label24"
+        Me.Label24.Size = New System.Drawing.Size(29, 13)
+        Me.Label24.TabIndex = 65
+        Me.Label24.Text = "Field"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(194, 61)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(81, 19)
+        Me.Button2.TabIndex = 64
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label23
+        '
+        Me.Label23.AutoSize = True
+        Me.Label23.Location = New System.Drawing.Point(45, 233)
+        Me.Label23.Name = "Label23"
+        Me.Label23.Size = New System.Drawing.Size(19, 13)
+        Me.Label23.TabIndex = 63
+        Me.Label23.Text = "12"
+        '
+        'Label22
+        '
+        Me.Label22.AutoSize = True
+        Me.Label22.Location = New System.Drawing.Point(45, 220)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(19, 13)
+        Me.Label22.TabIndex = 62
+        Me.Label22.Text = "11"
+        '
+        'Label21
+        '
+        Me.Label21.AutoSize = True
+        Me.Label21.Location = New System.Drawing.Point(45, 207)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(19, 13)
+        Me.Label21.TabIndex = 61
+        Me.Label21.Text = "10"
+        '
+        'Label20
+        '
+        Me.Label20.AutoSize = True
+        Me.Label20.Location = New System.Drawing.Point(51, 194)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(13, 13)
+        Me.Label20.TabIndex = 60
+        Me.Label20.Text = "9"
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(51, 181)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(13, 13)
+        Me.Label19.TabIndex = 59
+        Me.Label19.Text = "8"
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(51, 168)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(13, 13)
+        Me.Label18.TabIndex = 58
+        Me.Label18.Text = "7"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(51, 155)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(13, 13)
+        Me.Label17.TabIndex = 57
+        Me.Label17.Text = "6"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(51, 142)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(13, 13)
+        Me.Label16.TabIndex = 56
+        Me.Label16.Text = "5"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(51, 129)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(13, 13)
+        Me.Label15.TabIndex = 55
+        Me.Label15.Text = "4"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(51, 116)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(13, 13)
+        Me.Label14.TabIndex = 54
+        Me.Label14.Text = "3"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(51, 103)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(13, 13)
+        Me.Label13.TabIndex = 53
+        Me.Label13.Text = "2"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(102, 233)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(13, 13)
+        Me.Label12.TabIndex = 52
+        Me.Label12.Text = "0"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(102, 220)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(13, 13)
+        Me.Label11.TabIndex = 51
+        Me.Label11.Text = "0"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(102, 207)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(13, 13)
+        Me.Label10.TabIndex = 50
+        Me.Label10.Text = "0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(102, 194)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(13, 13)
+        Me.Label9.TabIndex = 49
+        Me.Label9.Text = "0"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(102, 181)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(13, 13)
+        Me.Label8.TabIndex = 48
+        Me.Label8.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(102, 168)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(13, 13)
+        Me.Label7.TabIndex = 47
+        Me.Label7.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(102, 155)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(13, 13)
+        Me.Label6.TabIndex = 46
+        Me.Label6.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(102, 142)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 13)
+        Me.Label5.TabIndex = 45
+        Me.Label5.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(102, 130)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 44
+        Me.Label4.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(102, 116)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 43
+        Me.Label3.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(102, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 42
+        Me.Label2.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(61, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 41
+        Me.Label1.Text = "Results"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(136, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 19)
+        Me.Button1.TabIndex = 40
+        Me.Button1.Text = "Roll those dice!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(17, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(98, 20)
+        Me.TextBox1.TabIndex = 39
+        '
+        'Label37
+        '
+        Me.Label37.AutoSize = True
+        Me.Label37.Location = New System.Drawing.Point(102, 251)
+        Me.Label37.Name = "Label37"
+        Me.Label37.Size = New System.Drawing.Size(13, 13)
+        Me.Label37.TabIndex = 79
+        Me.Label37.Text = "0"
+        '
+        'Label38
+        '
+        Me.Label38.Location = New System.Drawing.Point(33, 251)
+        Me.Label38.Name = "Label38"
+        Me.Label38.Size = New System.Drawing.Size(31, 13)
+        Me.Label38.TabIndex = 78
+        Me.Label38.Text = "Total"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.Controls.Add(Me.Label37)
+        Me.Controls.Add(Me.Label38)
+        Me.Controls.Add(Me.Label36)
+        Me.Controls.Add(Me.Label35)
+        Me.Controls.Add(Me.Label34)
+        Me.Controls.Add(Me.Label33)
+        Me.Controls.Add(Me.Label32)
+        Me.Controls.Add(Me.Label31)
+        Me.Controls.Add(Me.Label30)
+        Me.Controls.Add(Me.Label29)
+        Me.Controls.Add(Me.Label28)
+        Me.Controls.Add(Me.Label27)
+        Me.Controls.Add(Me.Label26)
+        Me.Controls.Add(Me.Label25)
+        Me.Controls.Add(Me.Label24)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label23)
+        Me.Controls.Add(Me.Label22)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Field Bet Roller"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label36 As Label
+    Friend WithEvents Label35 As Label
+    Friend WithEvents Label34 As Label
+    Friend WithEvents Label33 As Label
+    Friend WithEvents Label32 As Label
+    Friend WithEvents Label31 As Label
+    Friend WithEvents Label30 As Label
+    Friend WithEvents Label29 As Label
+    Friend WithEvents Label28 As Label
+    Friend WithEvents Label27 As Label
+    Friend WithEvents Label26 As Label
+    Friend WithEvents Label25 As Label
+    Friend WithEvents Label24 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label23 As Label
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label37 As Label
+    Friend WithEvents Label38 As Label
+End Class
---
+++ Dice_FieldBet/Field Bet Roller.vb
@@ -0,0 +1,110 @@
+﻿Public Class Form1
+
+    Dim RolledNumber As Integer
+    Dim Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9, Num10, Num11, Num12 As Integer
+    Dim NotFieldNumber, SingleField, DoubleField, TripleField, TotalFieldRolls, TotalRolls As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim NumberofRolls As Integer
+
+        TotalRolls = TotalRolls + TextBox1.Text
+
+        For NumberofRolls = 1 To TextBox1.Text
+
+            RolledNumber = Int((Rnd() * 6) + 1) + Int((Rnd() * 6) + 1)
+
+            If RolledNumber = 2 Then
+                Num2 = Num2 + 1
+                DoubleField = DoubleField + 1
+            End If
+            If RolledNumber = 3 Then Num3 = Num3 + 1
+            If RolledNumber = 4 Then Num4 = Num4 + 1
+            If RolledNumber = 5 Then Num5 = Num5 + 1
+            If RolledNumber = 6 Then Num6 = Num6 + 1
+            If RolledNumber = 7 Then Num7 = Num7 + 1
+            If RolledNumber = 8 Then Num8 = Num8 + 1
+            If RolledNumber = 9 Then Num9 = Num9 + 1
+            If RolledNumber = 10 Then Num10 = Num10 + 1
+            If RolledNumber = 11 Then Num11 = Num11 + 1
+            If RolledNumber = 12 Then
+                Num12 = Num12 + 1
+                TripleField = TripleField + 1
+            End If
+
+            If RolledNumber = 5 Or RolledNumber = 6 Or RolledNumber = 7 Or RolledNumber = 8 Then NotFieldNumber = NotFieldNumber + 1
+            If RolledNumber = 3 Or RolledNumber = 4 Or RolledNumber = 9 Or RolledNumber = 10 Or RolledNumber = 11 Then SingleField = SingleField + 1
+
+
+            TotalFieldRolls = SingleField + (DoubleField * 2) + (TripleField * 3)
+
+            Label2.Text = Num2
+            Label3.Text = Num3
+            Label4.Text = Num4
+            Label5.Text = Num5
+            Label6.Text = Num6
+            Label7.Text = Num7
+            Label8.Text = Num8
+            Label9.Text = Num9
+            Label10.Text = Num10
+            Label11.Text = Num11
+            Label12.Text = Num12
+
+            Label29.Text = NotFieldNumber
+            Label30.Text = SingleField
+            Label31.Text = DoubleField
+            Label32.Text = TripleField
+            Label34.Text = TotalFieldRolls
+            Label36.Text = TotalFieldRolls - NotFieldNumber
+            Label37.Text = TotalRolls
+
+        Next NumberofRolls
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Randomize()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Num2 = 0
+        Num3 = 0
+        Num4 = 0
+        Num5 = 0
+        Num6 = 0
+        Num7 = 0
+        Num8 = 0
+        Num9 = 0
+        Num10 = 0
+        Num11 = 0
+        Num12 = 0
+
+        NotFieldNumber = 0
+        SingleField = 0
+        DoubleField = 0
+        TripleField = 0
+        TotalFieldRolls = 0
+        TotalRolls = 0
+
+        Label2.Text = Num2
+        Label3.Text = Num3
+        Label4.Text = Num4
+        Label5.Text = Num5
+        Label6.Text = Num6
+        Label7.Text = Num7
+        Label8.Text = Num8
+        Label9.Text = Num9
+        Label10.Text = Num10
+        Label11.Text = Num11
+        Label12.Text = Num12
+
+        Label29.Text = NotFieldNumber
+        Label30.Text = SingleField
+        Label31.Text = DoubleField
+        Label32.Text = TripleField
+        Label34.Text = TotalFieldRolls
+        Label36.Text = TotalFieldRolls - NotFieldNumber
+        Label37.Text = TotalRolls
+
+    End Sub
+
+End Class
